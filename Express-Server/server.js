9a2833b884225a4cfc6cd992a50776e29275bf5d
techcursed <31003460+TechCursed@users.noreply.github.com>
2022-11-17 22:23:52 +0530

    Update server.js

--- Express-Server/server.js
+++ Express-Server/server.js
@@ -1,19 +1,18 @@
-const express = require("express")                            //import express 
-app = express()                                               //assign app to express
+const express = require("express")                                                      //import express 
+app = express()                                                                        //assign app to express
 
-app.listen(3000, function(){                                   // listens to a specific port
+app.listen(3000, function(){                                                         // listens to a specific port
     console.log("server is running on port 3000")
 })
 
-
-app.get("/",function(request,response){                          // works as GET request
+app.get("/",function(request,response){                                           // works as GET request
     response.send("<h1>server is running on port 3000</h1>")          
 })
 
-app.get("/about",function(request,response){                       // different routes for request          
+app.get("/about",function(request,response){                                    // different routes for request          
     response.send("<h1>This is about page</h1>")           
 })
 
-app.get("/contact",function(request,response){                         // different routes for request 
+app.get("/contact",function(request,response){                                // different routes for request 
     response.send("<h1>This is contact page</h1>")          
 })
